c7af20daf188ba99465289b1491c87c3fba46b53
michaelrhj <mic_471@yahoo.com>
2020-04-04 22:06:01 +0100

    Reversed the input and checks if the reverse is equal to the original input

--- Palindrome Numbers/palindrome.py
+++ Palindrome Numbers/palindrome.py
@@ -1,4 +1,15 @@
-palindrome_input = input("Enter number ") #takes input from user
-reverse_palindrome = palindrome_input[::-1]
-if palindrome_input == reverse_palindrome:
-    print("Palindrome ye")
+checked = False
+while checked == False:
+    palindrome_input = input("Enter number ") #takes input from user
+    if len(palindrome_input)<=20: #checks if palindrome is an integer up to 20 digits
+        checked = True
+
+nextPalindrome = False
+
+while nextPalindrome == False:
+    palindrome_input = int(palindrome_input) + 1 #increments number by 1
+    palindrome_input = str(palindrome_input) #converts back to a string so that it can be reversed below
+    reverse_palindrome = palindrome_input[::-1] #reversed
+    if palindrome_input == reverse_palindrome: #checks if it is a palindrome
+        print("Next Palindrome is " + palindrome_input) #if palindrome then outputs it to the user
+        nextPalindrome = True #breaks loop
