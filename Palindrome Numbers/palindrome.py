35704cca2f39a97ee002af3ff90cb61987b85d65
michaelrhj <mic_471@yahoo.com>
2020-04-04 20:49:14 +0100

    Reverses input and checks whether input and reverse are the same

--- Palindrome Numbers/palindrome.py
+++ Palindrome Numbers/palindrome.py
@@ -1,1 +1,5 @@
-palindrome_input = input("Enter number ")
+palindrome_input = input("Enter number ") #takes input from user
+reverse_palindrome = palindrome_input[::-1] #reverses input
+
+if palindrome_input==reverse_palindrome: #checks if input and reverse are equal
+    print("This number is a palindrome")
